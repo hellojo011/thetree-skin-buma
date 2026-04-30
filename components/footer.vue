0272b086446b9296a3efa3b86f043092c8e52f97
hellojo011 <hellojo011@hotmail.co.kr>
2026-05-01 01:01:39 +0900

    라이선스 링크 추가

--- components/footer.vue
+++ components/footer.vue
@@ -6,7 +6,7 @@
                     <span v-html="$store.state.page.data.copyright_text" />
                     <span v-html="$store.state.config['wiki.footer_text']" />
 
-                    theseed-skin-buma by LiteHell, the tree engine by hyonsu <br />
+                    <a href="https://github.com/hellojo011/thetree-skin-buma">theseed-skin-buma</a> by LiteHell & hellojo011, the tree engine by hyonsu <br />
                     theseed-skin-buma is distributed under
                     <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License Version 3</a>
                 </p>
